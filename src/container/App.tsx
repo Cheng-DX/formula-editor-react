129fbbeebbb8da623d10470976cbdb02c6e9cc89
Cheng_DX <2337040896@qq.com>
2022-04-26 20:34:57 +0800

    chore: delete useless files,prepare to move core function

---
+++ src/container/App.tsx
@@ -0,0 +1,10 @@
+import 'uno.css'
+
+function App() {
+  return (
+    <div className="h-200px w-200px bg-red">
+      <strong>Hello World</strong>
+    </div>
+  )
+}
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import { URL, fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/formula-editor-react/',
+  plugins: [
+    react(),
+    Unocss({
+      configFile: 'uno.config.ts',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'docs',
+  },
 })
